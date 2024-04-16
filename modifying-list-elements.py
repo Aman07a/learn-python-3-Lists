22c4e1e41e3c52a1c41c8993eb37bd469097538c
Aman07a <aman16.aa17@gmail.com>
2024-04-16 10:38:39 +0200

    Added modifying-list-elements

---
+++ modifying-list-elements.py
@@ -0,0 +1,14 @@
+# Step 1: Define a list called garden_waitlist with customers in order
+garden_waitlist = ["Jiho", "Adam", "Sonny", "Alisha"]
+
+# Step 2: Replace "Adam" with "Calla" in the garden_waitlist
+garden_waitlist[1] = "Calla"
+
+# Display the garden_waitlist after replacing "Adam" with "Calla"
+print(garden_waitlist)
+
+# Step 3: Replace "Alisha" with "Alex" using negative indexing
+garden_waitlist[-1] = "Alex"
+
+# Display the final garden_waitlist after replacing "Alisha" with "Alex"
+print(garden_waitlist)
