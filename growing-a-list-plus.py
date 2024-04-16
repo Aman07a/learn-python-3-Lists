58716198726b28cae8e646e1e708b59cfe0271cc
Aman07a <aman16.aa17@gmail.com>
2024-04-16 09:59:14 +0200

    Added growing-a-list-plus

---
+++ growing-a-list-plus.py
@@ -0,0 +1,17 @@
+# Existing list of orders
+orders = ["daisy", "buttercup", "snapdragon", "gardenia", "lily"]
+
+# 1. Create a list called new_orders that contains the new orders "lilac" and "iris"
+new_orders = ["lilac", "iris"]
+
+# 2. Use + to create a new list called orders_combined that combines orders with new_orders
+orders_combined = orders + new_orders
+
+# Display the combined list of orders
+print("Combined orders:", orders_combined)
+
+# 3. Fix the broken_prices list by adding brackets [] around the integer to concatenate it with a list
+broken_prices = [5, 3, 4, 5, 4] + [4]
+
+# Display the fixed prices list
+print("Fixed prices:", broken_prices)
