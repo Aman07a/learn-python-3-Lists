e3038fb3d943123246605f02613dd3dd992f9162
Aman07a <aman16.aa17@gmail.com>
2024-04-16 17:00:36 +0200

    Added the-power-of-range

---
+++ the-power-of-range.py
@@ -0,0 +1,7 @@
+# Modify range_five_three to start at 5, increment by 3, and end before 15
+range_five_three = range(5, 15, 3)
+print(list(range_five_three))
+
+# Create a range called range_diff_five starting at 0, incrementing by 5, and ending before 40
+range_diff_five = range(0, 40, 5)
+print(list(range_diff_five))
