5c9a13c0d1665ea2e50644194b08f28aaaa6a7fe
Aman07a <aman16.aa17@gmail.com>
2024-04-16 16:51:02 +0200

    Added consecutive-lists-range

---
+++ consecutive-lists-range.py
@@ -0,0 +1,7 @@
+# Modify number_list to range from 0 up to (but not including) 9
+number_list = range(9)
+print(list(number_list))
+
+# Create a range from 0 to 7 and print it as a list
+zero_to_seven = range(8)
+print(list(zero_to_seven))
