612e4ebb5026f2671c655d12a8534cb3aafbe302
Aman07a <aman16.aa17@gmail.com>
2024-04-16 09:21:53 +0200

    Added list-multiple-data-types

---
+++ list-multiple-data-types.py
@@ -0,0 +1,14 @@
+# Existing list containing integers and strings
+ints_and_strings = [1, 2, 3, "four", "five"]
+
+# 1. Add an additional string to the end of the 'ints_and_strings' list
+ints_and_strings.append("six")
+
+# Display the updated list
+print(ints_and_strings)
+
+# 2. Create a new list called 'sam_height_and_testscore'
+sam_height_and_testscore = ["Sam", 67, 85.5, True]
+
+# Display the new list
+print(sam_height_and_testscore)
