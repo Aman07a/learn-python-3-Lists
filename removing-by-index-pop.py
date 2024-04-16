b1da95d899d652530e153494272a00a27d8c5eb0
Aman07a <aman16.aa17@gmail.com>
2024-04-16 16:34:00 +0200

    Added removing-by-index-pop

---
+++ removing-by-index-pop.py
@@ -0,0 +1,10 @@
+data_science_topics = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
+print(data_science_topics)
+
+# Remove "Python 3" using .pop()
+data_science_topics.pop()
+print(data_science_topics)
+
+# Remove "Algorithms" using .pop()
+data_science_topics.pop(3)
+print(data_science_topics)
