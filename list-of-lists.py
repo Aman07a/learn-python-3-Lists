8406d26b10a1c50483341abfaaca0db24dbcc575
Aman07a <aman16.aa17@gmail.com>
2024-04-16 10:56:46 +0200

    Added list-of-lists

---
+++ list-of-lists.py
@@ -0,0 +1,18 @@
+# Step 1: Add Vik and his height to the heights list
+heights = [["Jenny", 61], ["Alexus", 70], ["Sam", 67], ["Grace", 64]]
+
+# New student Vik with height 68 inches
+vik = ["Vik", 68]
+heights.append(vik)
+
+# Print the updated heights list
+print("Updated heights list with Vik:", heights)
+
+# Step 2: Create a new two-dimensional list called ages
+ages = [
+    ["Aaron", 15],
+    ["Dhruti", 16]
+]
+
+# Print the ages list
+print("Ages list:", ages)
