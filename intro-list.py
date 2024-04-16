f2a786544a6a181c9d6ce0d5ccd3f8fc13822bf0
Aman07a <aman16.aa17@gmail.com>
2024-04-16 09:09:28 +0200

    Added intro-list

---
+++ intro-list.py
@@ -0,0 +1,8 @@
+# Existing list of heights
+heights = [61, 70, 67, 64]
+
+# 1. Add Chloe's height (65 inches) to the end of the 'heights' list
+heights.append(65)
+
+# 2. Fix the 'broken_heights' list by adding commas between the numbers
+broken_heights = [65, 71, 59, 62]
